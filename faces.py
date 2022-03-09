c89f4e4ae5d20eed35d4158142bea588729c5a29
PranavSindhanuru <pranavsindhanuru@gmail.com>
2022-03-09 20:08:15 +0530

    added rectangle

--- faces.py
+++ faces.py
@@ -11,8 +11,14 @@ while(True):
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for (x, y, w, h) in faces:
         roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
         img_item = 'ai_ml_project/my_img.png'
         cv2.imwrite(img_item, roi_gray)
+
+        color = (255, 255, 0)
+        stroke = 2
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color, stroke)
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
