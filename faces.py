25b3e1650ffb30e29f6c26c1e26b5fe6628ea2b0
PranavSindhanuru <pranavsindhanuru@gmail.com>
2022-03-13 21:17:58 +0530

    basic working model

--- faces.py
+++ faces.py
@@ -1,14 +1,27 @@
 import numpy as np
 import cv2
 import os
+import pickle
 # ↑ above is all import files ↑
 
+# using opencv recognizer for this run
+recognizer = cv2.face.LBPHFaceRecognizer_create()  # pip install opencv-contrib-python | to make it work
+recognizer.read('LBPH_train.yml')
+labels = dict()
+with open('labels.pickel', 'rb') as f:  # reading from pickle 
+    file_lables = pickle.load(f)
+    # reversing the file lables cuz its name : id, we want id : name cuz we have id from yml file but we need to show name
+    labels = {v:k for k, v in file_lables.items()} 
+
+
+
+
 # getting file dir cuz path is diffecrent in our PCs
-file_directory = os.getcwd().replace('\\', '/')
+file_directory = os.path.dirname(os.path.abspath(__file__))
 #using opencv frontal face template to recognize face (note: only detects front face)
-face_cascade = cv2.CascadeClassifier(f'{file_directory}/cascades/data/haarcascade_frontalface_alt2.xml')
+face_cascade = cv2.CascadeClassifier(f'{file_directory}\\cascades\\data\\haarcascade_frontalface_alt2.xml')
 # ↓ this takes pics ↓
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read() # taking images frame by frame
@@ -17,7 +30,17 @@ while(True):
     for (x, y, w, h) in faces:
         # print(x, y, w, h)
         roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]  # ROI stands for region of interest 
+
+        # using opencv train instead of scikit learn here ↓
+        id, confidence = recognizer.predict(roi_gray) # confidence is the predicted label !!! still need to figure out how it works 
+        if  confidence >= 45: # and confidence <= 85: 
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id]
+            color = (255, 0, 0)
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+
 
         cv2.imwrite('ai_ml_project/my_img.png', roi_gray) # saves detected faces
 
